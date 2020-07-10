73118303f4ca02bb83ea40276a994e5fbe0535f7
noel jeremy diatta <jeremynoeldiatta@gmail.com>
2020-07-10 16:36:44 +0000

    rendu TP JS amélioré

--- index.js
+++ index.js
@@ -23,41 +23,29 @@ typecli.addEventListener('change', function(e) {
          }
              
         });
-
         var typeco = document.getElementById("tco");
-        var afficher2 = document.getElementById("open1");
-        var cacher1 = document.getElementById("agios");
-        var ta = document.getElementById("agio")
-        var cacher2 = document.getElementById("frais");
-        var tf = document.getElementById("frai")
-        typeco.addEventListener('change', function(e) {
-        
-            if (typeco.value == "1" || typeco.value == "3") {
-                afficher2.style.display = 'block';
-                cacher2.style.display = 'inline-block';
-                tf.style.display = 'inline';
-                cacher1.style.display = 'none';
-                ta.style.display = 'none';
-                
-            } else {
-                    afficher2.style.display = 'block';
-                    cacher1.style.display = 'inline-block';
-                    ta.style.display = 'inline';
-                    cacher2.style.display = 'none';
-                    tf.style.display = 'none';
-                    
-               
-            
-            }
-        });
-        var myform = document.getElementById("ouverture");
-        var inputs = document.getElementsByTagName('input');
-        myform.addEventListener('submit',function(e){
-            if(!inputs.value){
-                e.preventDefault();
-                alert('veuiller remplir les champs');
-            }else{
+var afficher = document.getElementById("open1");
+var cacher1 = document.getElementById("agios");
+var ta = document.getElementById("agio")
+var cacher2 = document.getElementById("frais");
+var tf = document.getElementById("frai")
+typeco.addEventListener('change', function(e) {
 
-            }
-        });
-    
+    if (typeco.value == "1" || typeco.value == "3") {
+        afficher.style.display = 'block';
+        cacher2.style.display = 'inline-block';
+        tf.style.display = 'inline';
+        cacher1.style.display = 'none';
+        ta.style.display = 'none';
+        
+    } else {
+            afficher.style.display = 'block';
+            cacher1.style.display = 'inline-block';
+            ta.style.display = 'inline';
+            cacher2.style.display = 'none';
+            tf.style.display = 'none';
+         
+    }
+});
+alert('Bienvenue vous êtes raison de vivre !!!');
+ 
